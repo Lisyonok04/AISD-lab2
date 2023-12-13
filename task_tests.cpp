73988e11915ad500c3f6a69149d40529a54d3959
Mary <marybelka04@gmail.com>
2023-12-13 06:47:25 +0400

    Update task_tests.cpp

--- task_tests.cpp
+++ task_tests.cpp
@@ -5,3 +5,19 @@
 
 using namespace std;
 
+TEST(TaskTests, pushingpopingcheck1) {
+    LinkedList<int> a = LinkedList<int>(3, 5);
+    PrintList(a);
+    Node b = Node(2);
+    a.PushTail(b);
+    PrintList(a);
+    a.PushHead(b);
+    PrintList(a);
+    a.PopHead();
+    PrintList(a);
+    a.PopTail();
+    PrintList(a);
+    LinkedList<int> c = LinkedList<int>(3);
+    a.PushTail(c);
+    PrintList(a);
+}
