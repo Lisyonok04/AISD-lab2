040a90743b21497e6661450c2ea1ba2ecce7d921
Mary <marybelka04@gmail.com>
2023-12-13 06:26:11 +0400

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -145,6 +145,14 @@ public:
     {
         this->_head = other;
     }
+    Node<T>* GetTail() const
+    {
+        return this->_tail;
+    }
+    void SetTail(Node<T>* other)
+    {
+        this->_tail = other;
+    }
     void Swap(LinkedList<T>& other)
     {
         std::swap(this->_head, other._head);
@@ -277,3 +285,86 @@ public:
         return *this;
     }
 };
+
+template <class T>
+void sum_num(LinkedList<T>* lhs, LinkedList<T>* rhs) {
+    LinkedList<int> t;
+    int of = 0;
+    Node<int>* rt = rhs->GetTail();
+    Node<int>* lt = lhs->GetTail();
+    while (rt != nullptr && lt != nullptr) {
+        int r = *(rt->get_val());
+        int l = *(lt->get_val());
+        if (r + l + of > 9) {
+            t.PushHead(r + l + of - 10);
+            of = 1;
+        }
+        else {
+            t.PushHead(r + l + of);
+            of = 0;
+        }
+        rt = rt->get_prev();
+        lt = lt->get_prev();
+    }
+    while (rt != nullptr) {
+        int r = *(rt->get_val());
+        if (r + of > 9) {
+            t.PushHead(r + of - 10);
+            of = 1;
+        }
+        else {
+            t.PushHead(r + of);
+            of = 0;
+        }
+        rt = rt->get_prev();
+    }
+    while (lt != nullptr) {
+        int l = *(lt->get_val());
+        if (l + of > 9) {
+            t.PushHead(l + of - 10);
+            of = 1;
+        }
+        else {
+            t.PushHead(l + of);
+            of = 0;
+        }
+        lt = lt->get_prev();
+    }
+    if (of == 1) {
+        t.PushHead(1);
+    }
+    return t;
+}
+
+template <typename T>
+void mul_num(LinkedList<T>* lhs, LinkedList<T>* rhs) {
+    LinkedList<int> res;
+    res.PushHead(0);
+    Node<int>* rt = rhs->GetTail();
+    int count = 0;
+    while (rt != nullptr) {
+        int r = *(rt->get_val());
+        LinkedList<int> t;
+
+        int of = 0;
+        Node<int>* lt = lhs->GetTail();
+        while (lt != nullptr) {
+            int l = *(lt->get_val());
+            t.PushHead((l * r + of) % 10);
+            of = (l * r + of) / 10;
+            lt = lt->get_prev();
+        }
+        if (of != 0) {
+            t.PushHead(of);
+        }
+        for (int i = 0; i < count; i++) {
+            t.PushTail(0);
+        }
+
+        cout << t << "\n";
+        res = sum_num(&res, &t);
+        count++;
+        rt = rt->get_prev();
+    }
+    return res;
+}
