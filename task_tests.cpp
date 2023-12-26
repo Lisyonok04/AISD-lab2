3b95d3225a7e5a7e7b78f334683761756f41198d
Lisyonok04 <125237397+Lisyonok04@users.noreply.github.com>
2023-12-26 21:44:05 +0400

    Add files via upload

--- task_tests.cpp
+++ task_tests.cpp
@@ -5,24 +5,30 @@
 
 using namespace std;
 
+
 TEST(Tests, list1) {
     LinkedList list;
     Student student1("Smith", "John", 3, 4.5);
-    list.push_tail(&student1);
+    list.push_head(&student1);
     Student student2("Johnson", "Anna", 4, 2.8);
-    list.push_tail(&student2);
+    list.push_head(&student2);
     Student student3("Williams", "David", 2, 3.2);
-    list.push_tail(&student3);
+    list.push_head(&student3);
     Student student4("Brown", "Emily", 1, 2.9);
-    list.push_tail(&student4);
+    list.push_head(&student4);
     cout << "Students before deletion: " << endl;
     list.displayStudents();
     list.deleteLowGrades();
     cout << endl << "Students after deletion: " << endl;
     list.displayStudents();
+    cout << list.get_size();
+    LinkedList list_2;
+    list_2.push_head(&student1);
+    list_2.push_head(&list);
+    cout << "SUM" << endl;
+    list_2.displayStudents();
 }
-
-TEST(Tests, list2) {
+/*TEST(Tests, list2) {
     LinkedList list;
     Student student1("Smith", "John", 3, 4.5);
     list.push_tail(&student1);
@@ -34,7 +40,8 @@ TEST(Tests, list2) {
     list.push_tail(&student4);
     cout << "Students before deletion: " << endl;
     list.displayStudents();
+    list.pop_tail();
     list.deleteLowGrades();
     cout << endl << "Students after deletion: " << endl;
     list.displayStudents();
-}
+}*/
